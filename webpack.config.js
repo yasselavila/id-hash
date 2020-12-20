9c5c2ae249b8fe03d6be5daf191add3b3f445277
Yassel Avila <yassel.avila@gmail.com>
2020-12-20 09:26:28 -0500

    Update Copyright

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /**
  * @yag/id-hash
  *
- * @copyright Copyright (c) 2016 - 2018, Yassel Avila Gil (http://yasselavila.com)
+ * @copyright Copyright (c) 2016 - 2020, Yassel Avila Gil (https://twitter.com/yasselavila)
  * @license   BSD 3 Clause (see LICENSE.txt)
  * @link      https://github.com/yasselavila/js-id-hash
  */
